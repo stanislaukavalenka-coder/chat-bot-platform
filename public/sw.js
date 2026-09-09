3a0ba50ab4b3e0dbe9aa822372ac4420ad2816ed
stanislaukavalenka-coder <stanislaukavalenka@gmail.com>
2026-09-09 15:36:37 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const title = data.title || 'Новое сообщение';
+  const options = {
+    body: data.body || '',
+    icon: '/icon-192.png',
+    badge: '/badge-72.png',
+    vibrate: [200, 100, 200],
+    data: data.url || '/'
+  };
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data || '/')
+  );
+});
